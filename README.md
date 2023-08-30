248935c6bf5fafd5efff5d33e928781beef76547
tbello <bello4aus@gmail.com>
2023-08-30 13:41:44 +0100

    add static files

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Global Code Training
 > Guess who is tutoring flask
 - Yeah it is Tbello (*_-)
+
+# Problem I ran into
+- pip list --format=freeze > requirements.txt
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# To change the defualt template folder we use the argument template_folder='<name_of_folder>'
+# To change the default static folder we use the argument static_folder='<name_of_folder>'
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World Class Developers'
+    # return 'Hello World Class Developers'
+    return render_template('index.html')
 
 @app.route('/whereami')
 def whereami():
